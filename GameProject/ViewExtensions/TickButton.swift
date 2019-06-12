77e15661ae04c9b78585b115fc827b2004e84457
Annicha <annicha.dev@gmail.com>
2019-06-12 17:07:25 -0600

    Create tic-tac-toe grids

---
+++ GameProject/ViewExtensions/TickButton.swift
@@ -0,0 +1,15 @@
+//
+//  TickButton.swift
+//  GameProject
+//
+//  Created by Annicha Hanwilai on 6/12/19.
+//  Copyright © 2019 Annicha Hanwilai. All rights reserved.
+//
+
+import UIKit
+
+class TickButton: UIButton {
+
+    
+
+}
