b73dbf1aba67786fac25e27e93888a1e437c3bbb
Annicha <annicha.dev@gmail.com>
2019-06-12 12:27:08 -0600

    Initial commit

---
+++ GameProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameProject
+//
+//  Created by Annicha Hanwilai on 6/12/19.
+//  Copyright © 2019 Annicha Hanwilai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
