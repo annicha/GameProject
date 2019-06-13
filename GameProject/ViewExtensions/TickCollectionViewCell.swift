9c57fa2718652d0f05ae0e803f4031a438745b73
Annicha <annicha.dev@gmail.com>
2019-06-13 08:53:35 -0600

    Switches to collection view and connect button to each grid, position label can be access through button's positionLabel property.

---
+++ GameProject/ViewExtensions/TickCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TickCollectionViewCell.swift
+//  GameProject
+//
+//  Created by Annicha Hanwilai on 6/13/19.
+//  Copyright © 2019 Annicha Hanwilai. All rights reserved.
+//
+
+import UIKit
+
+class TickCollectionViewCell: UICollectionViewCell {
+    
+    var positionLabel: String?
+    var isClaimed: Bool = false
+    
+    lazy var tickButton: TickButton = {
+        let button = TickButton()
+        return button
+    }()
+    
+    
+    override func layoutSubviews() {
+        addSubview(tickButton)
+        
+        tickButton.anchor(top: self.topAnchor, bottom: self.bottomAnchor, leading: self.leadingAnchor, trailing: self.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeading: 0, paddingTrailing: 0)
+        tickButton.addTarget(self, action: #selector(tickButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func tickButtonTapped(){
+        guard let positionLabel = positionLabel else {return}
+        print("Tic button at \(positionLabel) tapped")
+    }
+}
---
+++ GameProject/ViewExtensions/UIViewExtension.swift
@@ -0,0 +1,44 @@
+//
+//  UIViewExtension.swift
+//  Palette
+//
+//  Created by Annicha Hanwilai on 6/11/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeading: CGFloat, paddingTrailing: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil){
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: paddingLeading).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: paddingTrailing).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+    }
+    
+} //extension ends
+
+
